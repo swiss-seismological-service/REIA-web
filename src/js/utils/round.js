40d2a036db6ae5d4001753e5c567f668c1968538
Nicolas Schmid <nicolas@breiten.ch>
2022-12-02 19:07:48 +0100

    feat: round function to precision including 0

--- src/js/utils/round.js
+++ src/js/utils/round.js
@@ -1,4 +1,13 @@
 export default function round(value, precision) {
     let multiplier = 10 ** (precision || 0);
-    return Math.round(value * multiplier) / multiplier;
+    let rounded = Math.round(value * multiplier) / multiplier;
+
+    if (Number.isInteger(rounded)) {
+        rounded = `${rounded.toString()}.`;
+        for (let i = 0; i < precision; i++) {
+            rounded = `${rounded}0`;
+        }
+    }
+
+    return rounded;
 }
