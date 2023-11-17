1d1655e529cc3d9cff9299c3153fb477ecd817d1
romanoe <noemi.romano@protonmail.com>
2023-11-17 11:56:58 +0100

    refactoring, error handling

--- src/js/webcomponents/DataLoader.js
+++ src/js/webcomponents/DataLoader.js
@@ -1,55 +1,53 @@
-/* eslint-disable no-console */
-
-import { getAllRiskAssessments, getLoss, getDamage, getCantonalInjuries, getCantonalStructuralDamage } from "../utils/api";
-
+import { getAllRiskAssessments, getLoss, getCantonalInjuries, getCantonalStructuralDamage } from "../utils/api";
 
 class DataLoader extends HTMLElement {
-    constructor() {
-        super();
-        this.promises = [];
-    }
-
-    // component attributes
+  
     static get observedAttributes() {
         return ['originId', 'baseUrl'];
     }
-    
-    
+
     connectedCallback() {
+        const originId = this.getAttribute('originId');
+        const baseUrl = this.getAttribute('baseUrl');
 
-        // Load data for a given origin id
-        // eslint-disable-next-line max-len
-        let riskAssessmentData = getAllRiskAssessments(20, 0, this.getAttribute('originId'), this.getAttribute('baseUrl'))
-                        // eslint-disable-next-line max-len
-                        .then((data) => data.items.slice(-1));
-        
-        riskAssessmentData.then((data) => {
+        if (!originId || !baseUrl) {
+            console.error('Missing required attributes: originId or baseUrl');
+            return;
+        }
 
-            // Get the loss calculation oid
-            let lossCalculationOid = data.map(d => d.losscalculation._oid)[0];
-            let damageCalculationOid = data.map(d => d.damagecalculation._oid)[0];
+        const fetchRiskAssessmentData = async () => {
+            try {
+                const data = await getAllRiskAssessments(20, 0, originId, baseUrl);
+                return data.items.slice(-1);
+            } catch (error) {
+                console.error('Error fetching risk assessment data:', error);
+                return [];
+            }
+        };
 
+        fetchRiskAssessmentData()
+            .then((data) => {
+                if (data.length > 0) {
+                    const lossCalculationOid = data[0].losscalculation._oid;
+                    const damageCalculationOid = data[0].damagecalculation._oid;
 
-            // Loss scales
-            document.querySelectorAll('loss-scale').forEach((lossScale) => {
-                // eslint-disable-next-line max-len
-                lossScale.setData(getLoss(lossCalculationOid,lossScale.getAttribute('losscategory'), 'Canton', null, false, this.getAttribute('baseUrl')));
+                    const setLossData = (lossScale) => {
+                        lossScale.setData(
+                            getLoss(lossCalculationOid, lossScale.getAttribute('losscategory'), 'Canton', null, false, baseUrl)
+                        );
+                    };
+
+                    document.querySelectorAll('loss-scale').forEach(setLossData);
+                    document.querySelector('loss-graph').setData(getCantonalInjuries(lossCalculationOid, baseUrl));
+                    document.querySelector('damage-graph').setData(getCantonalStructuralDamage(damageCalculationOid, baseUrl));
+                } else {
+                    console.warn('No data available for the provided originId.');
+                }
             })
-
-            // Loss graph
-            // eslint-disable-next-line max-len
-            document.querySelector('loss-graph').setData(getCantonalInjuries(lossCalculationOid, this.getAttribute('baseUrl')))
-          
-
-            // Damage graph
-            // eslint-disable-next-line max-len
-            document.querySelector('damage-graph').setData(getCantonalStructuralDamage(damageCalculationOid, this.getAttribute('baseUrl')));
-            ;
-
-        });
-        
+            .catch((error) => {
+                console.error('Error processing fetched data:', error);
+            });
     }
-    }
-     
-    
+}
+
 customElements.define('data-loader', DataLoader);
