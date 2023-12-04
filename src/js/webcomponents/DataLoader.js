f4a501a527a6e6bb83e791fb9b0766983cc9afbb
Noemi Romano <32194904+romanoe@users.noreply.github.com>
2023-12-04 10:05:39 +0100

    feat : add oid attribute + trigger the event when data is ready (#31)

--- src/js/webcomponents/DataLoader.js
+++ src/js/webcomponents/DataLoader.js
@@ -10,6 +10,7 @@ class DataLoader extends HTMLElement {
         super();
         this.originid = null;
         this.baseurl = null;
+        this.promises = [];
         this.lossScales = document.querySelectorAll('loss-scale');
         this.lossGraph = document.querySelector('loss-graph');
         this.damageGraph = document.querySelector('damage-graph');
@@ -19,27 +20,33 @@ class DataLoader extends HTMLElement {
         return ['originid', 'baseurl'];
     }
 
-    connectedCallback() {
-        this.updateData();
-    }
-
     attributeChangedCallback(property, oldValue, newValue) {
         if (oldValue === newValue) return;
         this[property] = newValue;
-        this.updateData();
+
+        if (property === 'originid' || property === 'baseurl') {
+            this.updateData();
+        }
     }
 
     async updateData() {
         if (this.originid && this.baseurl) {
             try {
                 const data = await this.fetchRiskAssessmentData();
-
                 if (data) {
                     const lossCalculationOid = data.losscalculation._oid;
                     const damageCalculationOid = data.damagecalculation._oid;
+                    const oid = data._oid;
+
                     this.updateLossScales(lossCalculationOid);
                     this.updateLossGraph(lossCalculationOid);
                     this.updateDamageGraph(damageCalculationOid);
+                    this.setAttribute('oid', oid);
+
+                    Promise.all(this.promises).then(() => {
+                        const event = new CustomEvent('data-ready', { bubbles: true });
+                        this.dispatchEvent(event);
+                    });
                 }
             } catch (error) {
                 console.error('Error fetching or updating data:', error);
@@ -51,47 +58,62 @@ class DataLoader extends HTMLElement {
         if (this.lossScales.length > 0) {
             this.lossScales.forEach((lossScale) => {
                 const lossCategory = lossScale.getAttribute('losscategory');
-                lossScale.setData(
-                    getLoss(lossCalculationOid, lossCategory, 'Canton', null, true, this.baseurl)
+                const loss = getLoss(
+                    lossCalculationOid,
+                    lossCategory,
+                    'Canton',
+                    null,
+                    true,
+                    this.baseurl
                 );
+                lossScale.setData(loss);
+                this.promises.push(loss);
             });
         }
     }
 
     updateLossGraph(lossCalculationOid) {
         if (this.lossGraph) {
-            this.lossGraph.setData(getCantonalInjuries(lossCalculationOid, this.baseurl));
+            let cantonalInjuries = getCantonalInjuries(lossCalculationOid, this.baseurl);
+            this.lossGraph.setData(cantonalInjuries);
+            this.promises.push(cantonalInjuries);
         }
     }
 
     updateDamageGraph(damageCalculationOid) {
         if (this.damageGraph) {
-            this.damageGraph.setData(
-                getCantonalStructuralDamage(damageCalculationOid, this.baseurl)
+            let cantonStructuralDamage = getCantonalStructuralDamage(
+                damageCalculationOid,
+                this.baseurl
             );
+            this.damageGraph.setData(cantonStructuralDamage);
+            this.promises.push(cantonStructuralDamage);
         }
     }
 
     async fetchRiskAssessmentData() {
         try {
-            const data = await getAllRiskAssessments(20, 0, this.originid, this.baseurl);
-            const preferred = data.items.filter((item) => item.preferred && item.published);
-
-            if (preferred.length > 1) {
-                return preferred.reduce((latest, current) =>
-                    new Date(current.creationinfo.creationtime) >
-                    new Date(latest.creationinfo.creationtime)
-                        ? current
-                        : latest
-                );
-            }
+            const riskAssessments = await getAllRiskAssessments(
+                100,
+                0,
+                this.originid,
+                this.baseurl
+            );
+            const preferred = riskAssessments.items.filter(
+                (item) => item.preferred && item.published
+            );
 
             if (preferred.length === 0) {
                 console.warn('No published and preferred risk assessment found for this origin id');
                 return null;
             }
 
-            return preferred[0];
+            return preferred.reduce((latest, current) =>
+                new Date(current.creationinfo.creationtime) >
+                new Date(latest.creationinfo.creationtime)
+                    ? current
+                    : latest
+            );
         } catch (error) {
             console.error('Error fetching risk assessment data:', error);
             throw error;
